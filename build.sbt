1744c899c863deb04d3f6e6b9c42154ee3f93a0f
Harrison Houghton <hora.rhino@gmail.com>
2020-04-16 19:22:34 -0400

    demo

---
+++ build.sbt
@@ -0,0 +1,3 @@
+enablePlugins(ScalaJSPlugin)
+scalacOptions ++= List("-Werror","-Wdead-code")
+scalaVersion := "2.13.1"
---
+++ src/main/scala/test.scala
@@ -0,0 +1,8 @@
+package fnord
+import scala.scalajs._, js.annotation._
+@JSGlobal("Blah")
+@js.native
+object Blah extends js.Object {
+
+  def blah(): Unit = js.native
+}
